8202601e932e2b8b234559fb5785e39052b2bcf2
solohad1n <fox663399@gmail.com>
2022-06-09 14:56:34 +0600

    the main logic of the game has been done: choosing the correct answer, passing through the cells, highlighting the moves.

--- src/components/BoardItem.vue
+++ src/components/BoardItem.vue
@@ -1,27 +1,43 @@
 <template>
-  <div class="board-item">
-    {{icon ? icon: ''}}
+  <div :class="'board-item ' + (selected ? 'selected ' : '') + (closed ? ' closed' : '')">
+    {{ icon ? icon : '' }}
   </div>
 </template>
 
 <script>
 export default {
   name: 'BoardItem',
-
+  
   props: {
     icon: {
-    type: Number,
-    required: false,
-    default: 0,
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    selected: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    closed: {
+      type: Boolean,
+      required: false,
+      default: false,
     }
   }
 }
 </script>
 
 <style>
-  .board-item{
-    border: 1px solid #2c3e50;
-    width: 48px;
-    height: 48px;
-  }
+.board-item {
+  border: 1px solid #2c3e50;
+  width: 48px;
+  height: 48px;
+}
+.selected {
+  background: #00ff0077;
+}
+.closed {
+  background: #00ff0022;
+}
 </style>
